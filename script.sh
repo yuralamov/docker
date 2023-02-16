a5abb3eff06686ce2e0158f80eb01a953a79eb7f
Yura Lamov <44276956+yuralamov@users.noreply.github.com>
2023-02-16 10:47:17 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,50 +11,24 @@ echo 'docker' > /etc/hosts
 # Настройка памяти
 swapfl=`swapon --show --noheadings | awk '{print $1}'`
 swapoff $swapfl
-fallocate -l 8G $swapfl
+fallocate -l 4G $swapfl
 chmod 600 $swapfl
 mkswap $swapfl
 swapon $swapfl
 
-# Настройка сети
-netfl=`ls /etc/netplan/`
-{
-echo 'network:'
-echo '  ethernets:'
-echo '    eth0:'
-echo '      dhcp4: false'
-echo '      addresses:'
-echo '      - 192.168.0.20/24'
-echo '      gateway4: 192.168.0.1'
-echo '      nameservers:'
-echo '        addresses:'
-echo '        - 192.168.0.1'
-echo '        - 8.8.8.8'
-echo '        search:'
-echo '        - WORKGROUP'
-echo '  version: 2'
-} > /etc/netplan/$netfl
-netplan generate
-netplan --debug apply
-
 # Настройка брандмауэра
-ufw allow OpenSSH
-ufw logging medium
-ufw allow 1433
-ufw allow 9000
-ufw enable
-ufw reload
-ufw status verbose
+ufw allow OpenSSH && ufw logging medium
+ufw enable && ufw reload && ufw status verbose
 
 # Установка docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io -y
+mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update && apt upgrade -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # Установка Porteiner
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker volume create portainer_data && docker pull portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 # Установка MS SQL Server 2017
 docker pull mcr.microsoft.com/mssql/server:2017-latest
@@ -66,4 +40,4 @@ docker restart sql2017
 
 exit
 sudo usermod -aG docker $(whoami)
-exit
+sudo reboot
